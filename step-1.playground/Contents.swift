5b461310c9d34596eebf5dbaf7d35733494f6e8a
Yang jun hyeok <yangsin1031@naver.com>
2020-12-09 00:17:28 +0900

    Solve the step-1
    As a result, finished the word push method

--- step-1.playground/Contents.swift
+++ step-1.playground/Contents.swift
@@ -1,1 +1,34 @@
+func wordPush( word:String, number:Int, direction: String) -> String {
+    var wordDict = [Int:String]()
+    var wordArray = word.map{String($0)}
+    var answer = [String]()
+    
+    if number%wordArray.count == 0 {
+        return wordArray.joined()
+    }
+    
+    if direction.uppercased() == "R" {
+        for i in 0..<wordArray.count {
+             
+            if i - number < 0 {
+                wordDict[i] = wordArray[(i-number) % wordArray.count + wordArray.count]
+            } else {
+                wordDict[i] = wordArray[(i-number) % wordArray.count]
+            }
+        }
+    } else if direction.uppercased() == "L" {
+        for i in 0..<wordArray.count {
+            if i + number > 0 {
+                wordDict[i] = wordArray[(i+number) % wordArray.count]
+            } else {
+                wordDict[i] = wordArray[(i+number) % wordArray.count + wordArray.count]
+            }
+        }
+    }
+    for (key, value) in wordDict.sorted{ $0 < $1 } {
+        answer.append(value)
+    }
+    return answer.joined()
+}
+
 
