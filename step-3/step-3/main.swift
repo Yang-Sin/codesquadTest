d5ea95d1c0dece37de412b5ec97015a1a00f9c17
Yang jun hyeok <yangsin1031@naver.com>
2020-12-09 21:37:39 +0900

    Make new step-3 file
    Make step-3 file and print initial cube

---
+++ step-3/step-3/main.swift
@@ -0,0 +1,20 @@
+if let input = readLine() {
+print("               B B B")
+print("               B B B")
+print("               B B B")
+
+print("W W W     O O O     G G G     Y Y Y")
+print("W W W     O O O     G G G     Y Y Y")
+print("W W W     O O O     G G G     Y Y Y")
+
+print("               R R R")
+print("               R R R")
+print("               R R R")
+
+print("CUBE> \(input)")
+
+        
+        
+    }
+    
+}
