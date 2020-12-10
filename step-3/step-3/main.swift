4f244956d2f7799fe76b8f06abb120e2924cba22
Yang jun hyeok <yangsin1031@naver.com>
2020-12-10 14:59:39 +0900

    Make rotate L and L' method
    Make roate L and L' method as same as rotate U or U' method

--- step-3/step-3/main.swift
+++ step-3/step-3/main.swift
@@ -13,7 +13,7 @@ if let input = readLine() {
     
     print("CUBE> \(input)")
     
-    struct movingCube {
+    struct MovingCube {
         var cube = [[["B", "B", "B"], ["B", "B", "B"], ["B", "B", "B"]], [["W", "W", "W"], ["W", "W", "W"], ["W", "W", "W"]], [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]], [["G", "G", "G"], ["G", "G", "G"], ["G", "G", "G"]], [["Y", "Y", "Y"], ["Y", "Y", "Y"], ["Y", "Y", "Y"]], [["R", "R", "R"], ["R", "R", "R"], ["R", "R", "R"]]]
         
         
@@ -41,6 +41,37 @@ if let input = readLine() {
                 cube[4][0][i] = cube2[3][0][i]
             }
         }
+        mutating func rotateLCube() {
+            let cube2 = cube
+            for i in 0...2 {
+//                [0][0] [0][1] [0][2] = [2][0] [1][0] [0][0]
+//                [1][0] [1][1] [1][2] = [2][1] [1][1] [0][1]
+//                [2][0] [2][1] [2][2] = [2][2] [1][2] [0][2]
+                cube[4][i][0] = cube2[4][2][i]
+                cube[4][i][1] = cube2[4][1][i]
+                cube[4][i][2] = cube2[4][0][i]
+                cube[0][0][i] = cube2[3][0][i]
+                cube[1][0][i] = cube2[0][0][i]
+                cube[3][0][i] = cube2[5][0][i]
+                cube[5][0][i] = cube2[1][0][i]
+            }
+        }
+        mutating func rotateL_Cube() {
+            let cube2 = cube
+            for i in 0...2 {
+//                [0][0] [0][1] [0][2] = [2][0] [1][0] [0][0]
+//                [1][0] [1][1] [1][2] = [2][1] [1][1] [0][1]
+//                [2][0] [2][1] [2][2] = [2][2] [1][2] [0][2]
+                cube[4][0][i] = cube2[4][i][2]
+                cube[4][1][i] = cube2[4][i][1]
+                cube[4][2][i] = cube2[4][i][2]
+                cube[0][0][i] = cube2[1][0][i]
+                cube[1][0][i] = cube2[5][0][i]
+                cube[3][0][i] = cube2[0][0][i]
+                cube[5][0][i] = cube2[3][0][i]
+            }
+        }
+        
     }
 }
 
